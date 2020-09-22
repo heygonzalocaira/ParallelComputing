4dd1e1bebcf5fc9cea8a074378c2620b8fd995ef
Gonzalo Caira <gc98wootz@gmail.com>
2020-09-21 23:33:30 -0500

    Added random matrix

---
+++ src/LAB 1  PRUEBAS CON LA MEMORIA CACHÉ/randomMatrix.cpp
@@ -0,0 +1,34 @@
+
+#include <bits/stdc++.h>
+
+int** random2dArray(unsigned height, unsigned width){
+    int** array2D = 0;
+    array2D = new int*[height];
+    for (int h = 0; h < height; h++){
+          array2D[h] = new int[width];
+          for (int w = 0; w < width; w++){
+                array2D[h][w] = 1+rand()%(1000-1);
+          }
+    }
+    return array2D;
+}
+int* random1dArray(unsigned width){
+    int* array1D = 0;
+    for (int w = 0; w < width; w++){
+        array1D[w] = 1+rand()%(1000-1);
+    }
+    return array1D;
+}
+
+int main(){
+    int** my2DArray = random2dArray(100, 100);
+    printf("Array sized [%i,%i] created.\n\n", 100,100);
+    for (int h = 0; h < 100; h++)
+      {
+            for (int w = 0; w < 100; w++)
+            {
+                  printf("%i,", my2DArray[h][w]);
+            }
+            printf("\n");
+      }
+}
