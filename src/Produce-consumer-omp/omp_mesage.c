a3f54ee2c59a82a9122e5be301625998fc9b106e
GonzaloCaira <gcairaaguilar@gmail.com>
2020-11-23 18:59:54 -0500

    comit

--- src/Produce-consumer-omp/omp_mesage.c
+++ src/Produce-consumer-omp/omp_mesage.c
@@ -18,19 +18,6 @@ struct queue{
     struct node* tail;
 };
 
-struct queue* struct node{
-    int src;
-    int mesg;
-    struct node* next;
-};
-
-struct queue{
-    int enqueue;
-    int dequeue;
-    struct node* front;  //frente de la cola
-    struct node* tail;
-};
-
 struct queue* Allocate(void){
     struct queue* q = malloc(sizeof(struct queue));
     q->enqueue = q->dequeue = 0;
@@ -38,53 +25,6 @@ struct queue* Allocate(void){
     q->tail = NULL;
     return q;
 }
-void Free(struct queue* q){
-    struct node* curr_p = q->front;
-    struct node* temp_p;
-
-    while(curr_p !=NULL){
-        temp_p =curr_p;
-        curr_p = curr_p->next;
-        free(temp_p);
-    }
-    q->enqueue = q->dequeue = 0;
-    q->front = q->tail = NULL;
-}
-
-void enqueue(struct queue* q, int src,int mesg){
-    struct node* n_p = malloc(sizeof(struct node));
-    n_p->src = src;
-    n_p->mesg = mesg;
-    n_p->next = NULL;
-    if( q->tail == NULL){
-        q->front = n_p;
-        q->tail = n_p;
-    }
-    else{
-        q->tail->next = n_p;
-        q->tail = n_p;
-    }
-    q->enqueue++;
-}
-
-int dequeue(struct queue* q, int* src_p,int* mesg_p){
-    struct node* temp_p;
-    if( q->front == NULL) return 0;
-    *src_p = q->front->src;
-    *mesg_p = q->front->mesg;
-    temp_p = q->front;
-
-    if ( q->front == q->tail){
-        q->front = q->tail = NULL;
-    }
-    else{
-        q->front = temp_p ->next;
-    }
-    free(temp_p);
-    q->dequeue++;
-    return 1;
-}
-
 void Free(struct queue* q){
     struct node* curr_p = q->front;
     struct node* temp_p;
@@ -139,8 +79,20 @@ int dequeue(struct queue* q, int* src_p,int* mesg_p){
     q->dequeue++;
     return 1;
 }
+int search(struct queue* q, int mesg,int* src_p){
+    struct node* curr_p = q->front;
 
-
+    while(curr_p != NULL){
+        if (curr_p-> mesg == mesg){
+            *src_p = curr_p->src;
+            return 1;
+        }
+        else{
+            curr_p = curr_p->next;
+        }
+    }
+    return 0;
+}
 void Send_msg(struct queue* msg_queue[],int my_rank);
 void Try_receive(struct queue* q,int my_rank);
 int Done(struct queue* q,int done_sending);
